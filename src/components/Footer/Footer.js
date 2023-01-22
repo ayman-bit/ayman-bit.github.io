0859ff087f385706f52ae2dbe022bb7935e3e893
Ayman Abu Awad <58958197+ayman-bit@users.noreply.github.com>
2023-01-22 01:45:26 -0330

    added components of website added app.js and css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/mosh-exe/portfolio'
+      className='link footer__link'
+    >
+      © 2022
+    </a>
+  </footer>
+)
+
+export default Footer
